3c25575c98aa83d93db98bd25e5b8f3b6a8c262f
Udit Kapoor <uditkapoor060@gmail.com>
2024-03-12 18:31:54 +0530

    Starting the project and setting up the appwrite

---
+++ src/conf/Conf.js
@@ -0,0 +1,10 @@
+// accessing the env variables sometimes shows an error or does not give app value
+const conf = {
+  appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
+  appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
