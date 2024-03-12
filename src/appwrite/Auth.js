038a88c54d56dfed917efb671473407221678b22
Udit Kapoor <uditkapoor060@gmail.com>
2024-03-12 19:17:03 +0530

    Setting up the authentication for the app

---
+++ src/appwrite/Auth.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-useless-catch */
+import conf from "../conf/Conf";
+import { Client, Account } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+
+  // as soon as someone creates objects of this class
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser() :: ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions;
+    } catch (error) {
+      console.log("Appwrite service :: logout() :: ", error);
+    }
+  }
+}
+
+// first one is the object and the second one is the class itself to call the constructor each time when object is constructed
+const authService = new AuthService();
+
+export default authService;
