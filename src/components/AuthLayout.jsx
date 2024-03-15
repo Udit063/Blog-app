57721d5cb3ce15c51427154627e687c6e4bd29c6
Udit Kapoor <uditkapoor060@gmail.com>
2024-03-16 01:25:12 +0530

    adding some basic components to the project

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 //will work when authentication is done and
 
 //when authentication is true then only it will render the pages otherwise don't render this page
 function Protected({ children, authentication = true }) {
-  return <>{children}</>;
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    // first authentication is req
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
+
+  return loader ? null : <>{children}</>;
 }
 
 export default Protected;
